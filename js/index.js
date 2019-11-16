836dc285894e56ae0aa0030d1e352f0593e7b809
minheekangg <minheekangg@gmail.com>
2019-11-16 17:15:55 -0500

    added less aggressive slow reveal

--- js/index.js
+++ js/index.js
@@ -16,18 +16,38 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
     var revealElements = document.getElementsByClassName("card");
-    console.log('cards are', revealElements)
+ 
     for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
-        var tween = TweenMax.fromTo(revealElements[i], 0.5, { yPercent: 100, opacity: 0.2 }, { yPercent: 0, opacity: 1, ease: Power1.easeInOut });;
+        var tween = TweenMax.fromTo(revealElements[i], 1, { yPercent: 20, opacity: 0.2 }, { yPercent: 0, opacity: 1, ease: Power1.easeInOut });;
         new ScrollMagic.Scene({
             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-			offset: -100,								 // start a little later
+			offset: 90,								 // start a little later
             triggerHook: 0.9,
         })
         .setTween(tween) 
         .addIndicators({ name: "digit " + (i + 1) }) // add indicators (requires plugin)
         .addTo(controller);
     }
+
+
+
+
+    
+    
+    // var projects = document.getElementsByClassName("project");
+    // var controller = new ScrollMagic.Controller({ vertical: false });
+    
+    // for (var i = 0; i < projects.length; i++) { // create a scene for each element
+    //     var tween = TweenMax.fromTo(projects[i], 0.5, { flexBasis: '450px', opacity: 0.2 }, { flexBasis: '400px', opacity: 1, ease: Power1.easeInOut });;
+    //     new ScrollMagic.Scene({
+    //         triggerElement: projects[i], // y value not modified, so we can use element as trigger as well
+    //         offset: 90,								 // start a little later
+    //         triggerHook: 0.9,
+    //     })
+    //         .setTween(tween)
+    //         .addIndicators({ name: "digit " + (i + 1) }) // add indicators (requires plugin)
+    //         .addTo(controller);
+    // }
 
 
 })
