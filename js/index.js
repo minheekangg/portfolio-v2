06805b67f9da2b9701c40ddd8fc2723d1a3901e6
minheekangg <minheekangg@gmail.com>
2019-11-16 17:00:53 -0500

    added scroll magic

--- js/index.js
+++ js/index.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
 
     // scroll to the right;
-    // init controller
-    // var controller = new ScrollMagic.Controller({ vertical: false });
+    var controller = new ScrollMagic.Controller();
 
     // // build tween
     // var tween = TweenMax.to(".project", 0.5, { backgroundColor: "green", width: "+=400" });
@@ -19,14 +18,15 @@ document.addEventListener('DOMContentLoaded', ()=>{
     var revealElements = document.getElementsByClassName("card");
     console.log('cards are', revealElements)
     for (var i = 0; i < revealElements.length; i++) { // create a scene for each element
+        var tween = TweenMax.fromTo(revealElements[i], 0.5, { yPercent: 100, opacity: 0.2 }, { yPercent: 0, opacity: 1, ease: Power1.easeInOut });;
         new ScrollMagic.Scene({
             triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-            offset: 90,												 // start a little later
-            // triggerHook: 0.9,
+			offset: -100,								 // start a little later
+            triggerHook: 0.9,
         })
-            .setTween(revealElements[i], 0.5, { marginTop: '200px' } ) // add class toggle
-            .addIndicators({ name: "digit " + (i + 1) }) // add indicators (requires plugin)
-            .addTo(controller);
+        .setTween(tween) 
+        .addIndicators({ name: "digit " + (i + 1) }) // add indicators (requires plugin)
+        .addTo(controller);
     }
 
 
